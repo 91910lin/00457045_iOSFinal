965099d4d7c4e8dbea4834ecf03b11091196c15c
“Your <goodjob132@yahoo.com.tw>
2019-06-24 17:26:40 +0800

    Initial Commit

---
+++ 00457045_HW3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  00457045_HW3
+//
+//  Created by User23 on 2019/6/24.
+//  Copyright © 2019 Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
